38a85e930442388fde698faee43b11cd4534cb9d
AndreyLinnik <av.linnik79@ya.ru>
2022-08-25 09:17:09 +0300

    homeWork5

---
+++ task34/Program.cs
@@ -0,0 +1,42 @@
+﻿int[] NewArray(int sise, int min, int value)
+{
+    int[] arr = new int[sise];
+    for (int i = 0; i < sise; i++)
+    {
+        arr[i] = new Random().Next(min, value);
+        
+
+    }
+
+    Console.WriteLine(String.Join(" ",arr));
+    return arr;
+}
+
+void EvenNum(int[] array)
+{
+    int res = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+       
+        if (array[i]%2==0)
+        {
+            res++;
+
+        }
+        
+        }
+
+    
+
+        Console.WriteLine("четных чисел:"+ res );
+        
+        
+}
+        
+    
+        
+        
+    
+
+int[] arr1= NewArray(10,100,300);
+EvenNum(arr1);
